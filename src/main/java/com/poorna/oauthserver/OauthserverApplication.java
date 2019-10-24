81ca3348778d5ddbc315db4894208735a428471a
Poorna Jayasinghe <poornasjayasinghe@gmail.com>
2019-10-25 10:00:06 +1100

    initial commit

---
+++ src/main/java/com/poorna/oauthserver/OauthserverApplication.java
@@ -0,0 +1,13 @@
+package com.poorna.oauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poorna/oauthserver/OauthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poorna.oauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
